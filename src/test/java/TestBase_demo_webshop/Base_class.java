111ca6c7d0d6d89d9ae480c4efc6ba9101adb1fd
Ashwinikcnfdl <ashwinik.hp@gmail.com>
2025-03-25 19:57:35 +0530

    demo_commit

--- src/test/java/TestBase_demo_webshop/Base_class.java
+++ src/test/java/TestBase_demo_webshop/Base_class.java
@@ -2,14 +2,11 @@ package TestBase_demo_webshop;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
